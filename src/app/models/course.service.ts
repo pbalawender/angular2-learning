d32077cb1a1f8b20f94471a4ca36abd859671df4
Przemyslaw Balawender <Przemyslaw_Balawender@epam.com>
2017-03-30 15:26:41 +0200

    pipes

--- src/app/models/course.service.ts
+++ src/app/models/course.service.ts
@@ -10,15 +10,37 @@ export class CourseService {
     this.courses = {};
 
     let course1 = new Course('Course #1', 30, moment().add(-7, 'days').toDate(),
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque condimentum leo consectetur sagittis eleifend. Donec nec tellus mollis, accumsan arcu vel, posuere eros. Morbi vel hendrerit urna. Nam auctor varius venenatis. Donec malesuada ante a vulputate sagittis. Donec consequat magna et nunc fermentum finibus. Proin sapien odio, malesuada in faucibus tempus, aliquam nec turpis.');
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. ' +
+      'Pellentesque condimentum leo consectetur sagittis eleifend. ' +
+      'Donec nec tellus mollis, accumsan arcu vel, posuere eros. ' +
+      'Morbi vel hendrerit urna. Nam auctor varius venenatis. ' +
+      'Donec malesuada ante a vulputate sagittis. ' +
+      'Donec consequat magna et nunc fermentum finibus. ' +
+      'Proin sapien odio, malesuada in faucibus tempus, aliquam nec turpis.', true);
     this.courses[course1.id] = course1;
 
     let course2 = new Course('Course #2', 60, moment().add(7, 'days').toDate(),
-      'Quisque consectetur neque sed nunc auctor consectetur. Duis non mollis purus, vel fermentum nunc. Nulla ut euismod nunc. Curabitur eu ex viverra, dapibus mauris quis, placerat nisi. Duis porta placerat lacus a elementum. Integer volutpat, nisl id posuere pellentesque, lectus arcu efficitur justo, vel fermentum risus arcu eu nisi. In sagittis lectus vitae risus molestie, non blandit sapien fermentum. Proin nec vestibulum erat. Praesent porta hendrerit magna ac consectetur. Donec rhoncus sed nibh ut rutrum.', true);
+      'Quisque consectetur neque sed nunc auctor consectetur. ' +
+      'Duis non mollis purus, vel fermentum nunc. ' +
+      'Nulla ut euismod nunc. Curabitur eu ex viverra, dapibus mauris quis, placerat nisi. ' +
+      'Duis porta placerat lacus a elementum. ' +
+      'Integer volutpat, nisl id posuere pellentesque, lectus arcu efficitur justo, ' +
+      'vel fermentum risus arcu eu nisi. In sagittis lectus vitae risus molestie, ' +
+      'non blandit sapien fermentum. Proin nec vestibulum erat. Praesent porta hendrerit ' +
+      'magna ac consectetur. Donec rhoncus sed nibh ut rutrum.');
     this.courses[course2.id] = course2;
 
     let course3 = new Course('Course #3', 90, moment().add(-16, 'days').toDate(),
-      'Vestibulum sed lectus non velit interdum venenatis. Sed vel quam vel nisl laoreet suscipit vel rutrum risus. Vivamus nec efficitur neque. Ut rhoncus dolor eu fringilla porta. Suspendisse ut quam sem. Fusce pellentesque ligula malesuada, vehicula tortor eget, tempor odio. Morbi vel neque id nisl vestibulum hendrerit. Aenean vulputate turpis vel turpis tempus placerat. Aenean quis consectetur lectus, sit amet dignissim lacus. Aenean hendrerit ornare ligula, auctor facilisis turpis. Vivamus porttitor sed tortor a dignissim. Curabitur vel turpis dignissim, ultrices ante sed, dignissim orci.');
+      'Vestibulum sed lectus non velit interdum venenatis. ' +
+      'Sed vel quam vel nisl laoreet suscipit vel rutrum risus. ' +
+      'Vivamus nec efficitur neque. Ut rhoncus dolor eu fringilla porta. ' +
+      'Suspendisse ut quam sem. Fusce pellentesque ligula malesuada, vehicula tortor eget, ' +
+      'tempor odio. Morbi vel neque id nisl vestibulum hendrerit. ' +
+      'Aenean vulputate turpis vel turpis tempus placerat. ' +
+      'Aenean quis consectetur lectus, sit amet dignissim lacus. ' +
+      'Aenean hendrerit ornare ligula, auctor facilisis turpis. ' +
+      'Vivamus porttitor sed tortor a dignissim. ' +
+      'Curabitur vel turpis dignissim, ultrices ante sed, dignissim orci.');
     this.courses[course3.id] = course3;
   }
 
